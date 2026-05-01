035ac73befb587a366e53a546a63b6cc5dc677c2
TakkunMC_jp <Kaneda.family.803@gmail.com>
2026-05-01 18:47:53 +0900

    Update preload.js

--- src/preload.js
+++ src/preload.js
@@ -1,9 +1,10 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('folderTreeAPI', {
-  selectFolder: () => ipcRenderer.invoke('select-folder'),
-  readFolder: (folderPath) => ipcRenderer.invoke('read-folder', folderPath),
-  saveFile: (content) => ipcRenderer.invoke('save-file', content),
-  getTheme: () => ipcRenderer.invoke('get-theme'),
-  copyToClipboard: (text) => ipcRenderer.send('copy-to-clipboard', text)
+  selectFolder:      () => ipcRenderer.invoke('select-folder'),
+  readFolder:        (folderPath) => ipcRenderer.invoke('read-folder', folderPath),
+  validateDropPath:  (folderPath) => ipcRenderer.invoke('validate-drop-path', folderPath),
+  saveFile:          (content) => ipcRenderer.invoke('save-file', content),
+  getTheme:          () => ipcRenderer.invoke('get-theme'),
+  copyToClipboard:   (text) => ipcRenderer.send('copy-to-clipboard', text)
 });
